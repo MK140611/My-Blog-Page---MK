7c1b61671ef64d8cc547fe833a29ded078d840de
MK <kelmeia@dunwoody.edu>
2025-12-09 11:52:31 -0600

    Website Breakthrough (Finally!)

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,16 @@ $(document).ready(function() {
     $('.menu-toggle').on('click', function(){
         $('.nav').toggleClass('showing');
         $('.nav ul').toggleClass('showing');
+      });
 
-    });
-
-//SLideShowww app//
-$(".post-wrapper").slick({
+//SLideShowww app slick//
+$('.post-wrapper').slick({
   slidesToShow: 3,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
-  nextArrow: $('next'),
-  prevArrow: $('prev'),
+  nextArrow: $('.next'),
+  prevArrow: $('.prev'),
 
   responsive: [
     {
