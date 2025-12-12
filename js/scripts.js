482316b82c8be856d112a2f0fa590a4a8a280447
MK <kelmeia@dunwoody.edu>
2025-12-12 15:11:15 -0600

    Final Changes????

--- js/scripts.js
+++ js/scripts.js
@@ -41,3 +41,47 @@ $('.post-wrapper').slick({
 });
 
 });
+
+const { ClassicEditor, Heading, BlockQuote, Bold, Italic, Font, Link, List } = CKEDITOR;
+
+ClassicEditor
+	.create( document.querySelector( '#body' ), {
+
+		toolbar: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'fontSize',
+			'fontFamily',
+			'fontColor',
+			'|',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'blockQuote'
+			],
+		heading: {
+			options: [
+				{ model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+				{ model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+				{ model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+			]
+		},
+		fontFamily: {
+			options: [
+				'default',
+				'Ubuntu, Arial, sans-serif',
+				'Ubuntu Mono, Courier New, Courier, monospace'
+			]
+		},
+		fontColor: {
+			colorPicker: {
+				// Use 'hex' format for output instead of 'hsl'.
+				format: 'hex'
+			}
+		},
+	} )
+	.catch( error => {
+		console.log( error );
+	} );
